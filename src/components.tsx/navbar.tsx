aea3e54f33fb76b92238ee470d43a5c3f2545d65
Simon Pohmann <simon@pohmann.de>
2025-07-21 15:15:32 +0100

    Designed website

---
+++ src/components.tsx/navbar.tsx
@@ -0,0 +1,27 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="mainNavbar">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#top">General</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#publications">Publications</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#talks">Talks</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#experience">Experience</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    );
+}
